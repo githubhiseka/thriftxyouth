e1ae7a841601ad6801cbddba68dbc82b39037f67
githubhiseka <susantoabhiseka@gmail.com>
2024-09-10 11:19:11 +0700

    made model and changed main.html

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'app_name': 'main',
+        'name': 'Abhiseka Susanto',
+        'class': 'C'
     }
 
     return render(request, "main.html", context)
