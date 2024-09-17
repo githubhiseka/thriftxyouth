f83880f224bcea210de5ce6c550d0fbfc4612da4
githubhiseka <susantoabhiseka@gmail.com>
2024-09-18 00:02:18 +0700

    basically done tugas 3

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.models import Product
 from main.forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     product_entries = Product.objects.all()
@@ -22,3 +24,19 @@ def create_product_entry(request):
 
     context = {'form': form}
     return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
