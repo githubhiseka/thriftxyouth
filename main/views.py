f23fb0669a95d9dd39e454fb8680bdcbda61c0cd
githubhiseka <susantoabhiseka@gmail.com>
2024-09-17 22:54:56 +0700

    change to other laptop

--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.models import Product
+from main.forms import ProductForm
 
 def show_main(request):
+    product_entries = Product.objects.all()
     context = {
         'app_name': 'THRIFTxYouth',
         'name': 'Abhiseka Susanto',
-        'class': 'C'
+        'class': 'C',
+        'npm': '2306244942',
+        'product_entries': product_entries
     }
-
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
