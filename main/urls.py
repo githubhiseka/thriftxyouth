d309d555fc8652fa3922f751bba545c260ef1657
githubhiseka <susantoabhiseka@gmail.com>
2024-10-09 08:21:25 +0700

    tinggal readme

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from main.views import show_main, create_product_entry, edit_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_product_entry, add_product_entry_ajax, edit_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
+    path('add-product-entry-ajax', add_product_entry_ajax, name='add_product_entry_ajax'),
     path('edit-product/<uuid:id>', edit_product, name='edit_product'),
     path('delete-product/<uuid:id>', delete_product, name='delete_product'),
     path('xml/', show_xml, name='show_xml'),
