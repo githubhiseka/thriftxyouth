22f98b9c6ab0af69c06c414870c65d00bf47edde
githubhiseka <susantoabhiseka@gmail.com>
2024-11-20 11:30:59 +0700

    django setup for flutter possibly done:

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
@@ -122,3 +124,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = Product.objects.create(
+            user=request.user,
+            product=data["name"],
+            product_intensity=int(data["price"]),
+            feelings=data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
